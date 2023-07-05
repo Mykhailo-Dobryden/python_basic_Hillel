decfce5b4abad1197369a03f0e92b4fb8931fca2
Mykhailo-Dobryden <dobrydenmykhail@gmail.com>
2023-07-05 21:51:43 +0300

    HW3 Task4 add second variant

--- home_work_3/task_4_numbers_multiples_5.py
+++ home_work_3/task_4_numbers_multiples_5.py
@@ -4,8 +4,17 @@ list_of_six = [100, 106, 112, 118, 124, 130, 136,
 
 Надрукувати всі числа, які діляться на 5 без залишку, але не більше 150."""
 
+# --------------First variant------------------
+
 list_of_six = list(range(100, 197, 6))
 
 for num in list_of_six:
     if (num % 5) == 0 and num < 150:
         print(num)
+
+
+# --------------Second variant------------------
+
+list_of_six = list(range(100, 197, 6))
+multiples_5 = [i for i in list_of_six if i % 5 == 0 and i < 150]
+print(f"Second variant: {multiples_5}")
