f73453819b2cac42b48794bd51a9b38b0194e8b1
Mykhailo-Dobryden <dobrydenmykhail@gmail.com>
2023-07-07 18:50:42 +0300

    HW4 task 1 updated

--- home_work_4/task_1_running_sportsman.py
+++ home_work_4/task_1_running_sportsman.py
@@ -4,21 +4,23 @@
 Програма отримує на вхід числа `x` та `y` і має вивести одне число - номер дня."""
 
 
-def return_marathon_day(a: int, b: int) -> int:
+def return_marathon_day(a: float, b: float) -> int:
     """a - is distance for first day, b - distance for desired day"""
+    distance = a
     day = 1
-    while b > a:
-        b *= 0.9
+    while distance < b:
+        distance += distance * 0.1
         day += 1
     return day
 
 
-def user_input(prompt: str = "Enter a number: ") -> int:
-    return int(input(prompt))
+def user_input(prompt: str = "Enter a number: ") -> float:
+    return float(input(prompt))
 
 
 number_x = user_input("Enter a number x: ")
 number_y = user_input("Enter a number y: ")
 
-print(return_marathon_day(number_x, number_y))
+num_of_day = return_marathon_day(number_x, number_y)
 
+print(f"The day number when the distance will be not less than {number_y} is: {num_of_day}")
