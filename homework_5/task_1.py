98cb4ed0b1f39c1799ae550eb390d0911172fdfe
Mykhailo-Dobryden <dobrydenmykhail@gmail.com>
2023-07-11 19:48:21 +0300

    HW5 task1

---
+++ homework_5/task_1.py
@@ -0,0 +1,21 @@
+"""1. Написати функцію `is_prime`, що приймає 1 аргумент - число від 0 до 1000,
+і повертає `True`, якщо воно просте, і `False` - інакше.
+(Прості числа - ті які діляться без залишку тільки на себе або 1,
+наприклад 2, 3, 5, 7, 11 ...)"""
+
+
+def is_prime(num):
+    if num < 0 or num > 1000:
+        return str(f"Number {num} is out of required range")
+    if num < 2:
+        return False
+
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+
+    return True
+
+
+simple_num = int(input("Enter number from 0 to 1000: "))
+print(is_prime(simple_num))
